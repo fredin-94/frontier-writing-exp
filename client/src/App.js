a81852734d5dae3da4ceb5a1fecb1c3642efdd8a
Lucas Fredin <ii.fredin@gmail.com>
2019-12-02 17:06:56 +0100

    basic login functions working

--- client/src/App.js
+++ client/src/App.js
@@ -7,27 +7,31 @@ import store from './store';
 import Navbar from './components/shared-layout/Navbar';
 import LandingPage from './components/shared-layout/LandingPage';
 import Register from './components/auth/Register';
-import Login from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 import setAuthToken from './utils/setAuthToken'; //
 import {setCurrentUser, logoutUser} from './actions/authActions'; //
 import PrivateRoutes from './components/private-routes/PrivateRoutes';
 import Homepage from './components/pages/Homepage';
 
+//jwt_decode, setauthtoken , setcurrentuser, logoutuser
+
 if(localStorage.jwtToken){
   const token = localStorage.jwtToken;
-  setAuthToken(jwtToken);
+  setAuthToken(token);
   const decoded = jwt_decode(token);
   store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = Date.now() / 1000; //milisec
+
+  if(decoded.exp < currentTime){
+    store.dispatch(logoutUser());
+
+    window.location.href = "./login";
+  }
 }
 
-const currentTime = Date.now() / 1000; //milisec
 
-if(decoded.exp < currentTime){
-  store.dispatch(logoutUser());
-
-  window.location.href = "./login";
-}
 
 
 class App extends Component {
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//choose our App component to put inside the element with id root (found in index.html in the public folder)
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
