6c0783ead511fae226097fbf5cb7ed0d612bd12a
Lucas Fredin <ii.fredin@gmail.com>
2020-04-09 10:39:43 +0200

    fixed chapter links

--- client/src/components/userPages/SelectedBook.js
+++ client/src/components/userPages/SelectedBook.js
@@ -13,7 +13,7 @@ class SelectedBook extends React.Component{
     
     state = {
         selectedChapter: '',
-        currentChapter: 1
+        currentChapter: 0
     }
 
     getBookIdFromUrl(){ //change to get this from utils instead
@@ -52,6 +52,20 @@ class SelectedBook extends React.Component{
         });
     }
 
+    setCurrentChapter = (e)=>{ /* doesnt work */
+        console.log("chapter value: ");
+        console.log(e.target);
+        console.log(e.target.id);
+        let chapterId = e.target.id;
+        for(let i = 0; i < this.props.books.selectedBookChaptersLength; i++){
+            if( this.props.books.selectedBook.chapters[i]._id == chapterId ){
+                this.setState({
+                    currentChapter: i
+                });
+            }
+        }
+    }
+
     //if there is a chapter selected, its contents should be displayed in the text editor 
     //so im thinking, have this site only to show backside text, edit delete etc, and only show text editor on another page after the chapter has been selected
 
@@ -62,7 +76,14 @@ class SelectedBook extends React.Component{
         const bookId = this.getBookIdFromUrl();
         if(book !== undefined && chapters!== undefined){
             const chaptersDisplayed = chapters.map((chapter)=>
-                <div key={chapter._id}><Link to={`/writeBook/${bookId}/${chapter._id}`} className="chapterList">{chapter.title}</Link></div>
+                <div key={chapter._id}>
+                    <li id={chapter._id} onClick={this.setCurrentChapter}>{chapter.title}</li> 
+                    {/* TODO: if user logged in is same as the author, enable edit */}
+                    <div>
+                        <Link to={`/writeBook/${bookId}/${chapter._id}`}>Edit</Link> 
+                    </div>
+                    <hr/>
+                </div>
             );
             return chaptersDisplayed;
         }
@@ -73,7 +94,7 @@ class SelectedBook extends React.Component{
 
     renderBookContent = ()=>{
         console.log(this.props.books.selectedBook.chapters);
-        const chapter = this.props.books.selectedBookChapters[this.state.currentChapter-1]; 
+        const chapter = this.props.books.selectedBookChapters[this.state.currentChapter]; 
         const book = this.props.books.selectedBook; 
         if(book !== undefined && chapter !== undefined){
                  return <div key={chapter.title} className="read-chapter-div">
@@ -97,15 +118,15 @@ class SelectedBook extends React.Component{
     }
 
     renderPrevBtn = ()=>{
-        if(this.state.currentChapter > 1){
-            return <a onClick={this.setChapterToDisplay}>Previous Chapter</a>
+        if(this.state.currentChapter > 0){
+            return <li onClick={this.setChapterToDisplay}>Previous Chapter</li>
         }
     }
 
     renderNextBtn = ()=>{
         let chapterLength = this.props.books.selectedBookChaptersLength;
-        if(chapterLength >= 1 && this.state.currentChapter !== chapterLength){
-            return <a className="next-chapter" onClick={this.setChapterToDisplay}>Next Chapter</a>
+        if(chapterLength >= 1 && this.state.currentChapter !== chapterLength-1){
+            return <li className="next-chapter" onClick={this.setChapterToDisplay}>Next Chapter</li>
         }
     }
 
