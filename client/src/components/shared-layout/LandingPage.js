b4270d7e7983a46b90f76bd831acbc0617f0bb1e
Lucas Fredin <ii.fredin@gmail.com>
2020-04-13 15:01:39 +0200

    making some styledcomponents to for try

--- client/src/components/shared-layout/LandingPage.js
+++ client/src/components/shared-layout/LandingPage.js
@@ -1,5 +1,27 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import styled from 'styled-components';
+
+const Button = styled.button`
+  cursor: pointer;
+  background-color: ${props => (props.login ? 'white' : 'black')};
+  color: ${props => (props.login ? 'black' : 'white')};
+  font-size: 15.5px;
+  font-family: 'Raleway', sans-serif;
+  border-radius: 2px;
+  margin: 0.2em 1em;
+  padding: 1.2em 2.5em;
+  transition: 0.5s all ease-out;
+  border: none;
+  box-shadow: 0 5px 5px 0 rgba(0,0,0,0.24),0 5px 5px 0 rgba(0,0,0,0.19);
+  width: 9em;
+
+  &:hover {
+    background-color: ${props => (props.login ? 'white' : 'black')};
+    color: ${props => (props.login ? 'black' : 'white')};
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+  }
+`;
 
 class LandingPage extends React.Component{
 
@@ -22,7 +44,6 @@ class LandingPage extends React.Component{
             this.history.push('/homepage');
 
         }
-
     }
    
     render(){
@@ -36,18 +57,14 @@ class LandingPage extends React.Component{
                         <hr/>
                         <p>New member? Sign up! Already a member? Sign in!</p>
                         
-                        <div className="btn-container col s12 m6">
-                            <Link to="/register" style={{ width: "140px"}}
-                                className="btn btn-large waves-effect waves-light black white-text hoverable">
-                                Sign Up
+                        <div>
+                            <Link to="/register" style={{ width: "140px"}}>
+                                <Button>SIGN UP</Button>
                             </Link>
-                        </div>
-                        
-                        <div className="btn-container col s12 m6">
-                            <Link to="/login" style={{ width: "140px"}}
-                                className="btn btn-large btn waves-effect white black-text">
-                                Log In
+                            <Link to="/login" style={{ width: "140px"}}>
+                                <Button login>SIGN IN</Button>
                             </Link>
+                            
                         </div>
                     
                     </div>
